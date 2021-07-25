8e381f52e76a6f1745b47e48256d17c4a2fc6456
facuevas <franciscuevas4@gmail.com>
2021-07-25 11:10:48 -0700

    setting up formik

--- client-app/src/features/activities/details/ActivityDetailedHeader.tsx
+++ client-app/src/features/activities/details/ActivityDetailedHeader.tsx
@@ -1,5 +1,6 @@
 import { observer } from "mobx-react-lite";
 import React from "react";
+import { Link } from "react-router-dom";
 import { Segment, Image, Item, Header, Button } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
 
@@ -47,7 +48,7 @@ function ActivityDetailedHeader({ activity }: Props) {
         <Segment clearing attached="bottom">
           <Button color="teal">Join Activity</Button>
           <Button>Cancel Attendence</Button>
-          <Button color="orange" floated="right">
+          <Button as={Link} to={`/manage/${activity.id}`} color="orange" floated="right">
             Manage Event
           </Button>
         </Segment>
