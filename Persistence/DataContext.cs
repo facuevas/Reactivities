83ecdd5f07bb67b447d3d98dd17a218e8cb02c11
facuevas <franciscuevas4@gmail.com>
2021-07-29 15:11:04 -0700

    Started section 12 on creating user accounts. Added AspNetCore.Identity middleware to help create accounts. Linked accounts to database

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -1,4 +1,5 @@
 using Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Persistence
@@ -6,7 +7,7 @@ namespace Persistence
     /*
         Uses EntityFramework core to create migrations and push to the database as well as create the database.
     */
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<AppUser>
     {
         public DataContext(DbContextOptions options) : base(options)
         {
